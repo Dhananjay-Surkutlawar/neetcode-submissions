e8e4d8c6f28e3642e07b434f1ff4619de6642074
Dhananjay Surkutlawar <136974072+Dhananjay-Surkutlawar@users.noreply.github.com>
2026-06-29 21:46:46 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+    
+    char [] s1 = s.ToCharArray();
+    char [] s2 = t.ToCharArray();
+    Array.Sort(s1);
+    Array.Sort(s2);
+    return new string(s1) == new string(s2);
+
+    }
+}
